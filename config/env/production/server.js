598059beb4945aa92d40971eb002dcd9cca55cee
Eric Whitcomb <eric.whitcomb@gmail.com>
2022-04-30 11:02:57 -0500

    Small fix for next attempt at deployment to heroku

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
+  proxy: true,
   host: env("HEROKU_APP_URL"),
   port: env.int("PORT"),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array("APP_KEYS", ["testkey1", "testkey2"]),
   },
 });
